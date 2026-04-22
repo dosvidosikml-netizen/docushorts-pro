0ada28a5218e9d4612b8e6ed4618db28867f5cc5
dosvidosikml-netizen <dosvidosikml@gmail.com>
2026-04-23 00:26:44 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { SYS_SCENE_ENGINE, buildSceneUserPrompt } from "@/engine/sceneEngine";
+import { SYS_SCENE_ENGINE, buildSceneUserPrompt } from "../engine/sceneEngine";
 
 export default function Page() {
   const [input, setInput] = useState("");
